afd063cc7039148f546fb3253ff605d4ade70bb3
LuisRe135 <64992020+LuisRe135@users.noreply.github.com>
2024-07-27 19:54:57 +0000

    first try card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const randomValue = arr => Math.floor(Math.random() * arr.length);
+const cardValue = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const suit = ["♦", "♥", "♠", "♣"];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let valueElement = document.querySelector("#cardValue");
+  valueElement.innerHTML = cardValue[randomValue(cardValue)];
+  let suitElement = document.querySelectorAll(".suit");
+  let selectedSuit = suit[randomValue(suit)];
+  if (selectedSuit == "♦" || selectedSuit == "♥") {
+    suitElement.forEach(element => {
+      element.classList.add("text-danger");
+    });
+  }
+  suitElement.forEach(element => {
+    element.innerHTML = selectedSuit;
+  });
 };
